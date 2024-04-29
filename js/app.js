42011d5e09b46a1dbd18077d1ddadbf18ceebc2b
Dize dev <axiozweb2022@gmail.com>
2024-04-29 17:19:01 -0500

    se termino el proyecto

--- js/app.js
+++ js/app.js
@@ -1,35 +1,58 @@
+const ul = document.querySelector("ul"),
+    input = document.querySelector("input"),
+    tagNumb = document.querySelector(".details span");
 
-const ul = document.querySelector('ul');
-const input = document.querySelector('input');
+let maxTags = 10,
+    tags = [];
 
-let tags = [];
+
+
+const countTags = () => {
+    input.focus();
+    tagNumb.innerText = maxTags - tags.length;
+}
+
 
 const createTag = () => {
-
-    [...ul.querySelectorAll('li')].every(li => li.remove());
-
-    console.log(tags.slice().reverse());
+    [...ul.querySelectorAll("li")].map(li => li.remove());
     tags.slice().reverse().some(tag => {
-        let liTag = `  <li>${tag} <i class="ri-close-fill"></i></li> `;
-        ul.insertAdjacentHTML('afterbegin', liTag)
+        let liTag = `<li>${tag} <i class="uit uit-multiply" onclick="remove(this, '${tag}')"></i></li>`;
+        ul.insertAdjacentHTML("afterbegin", liTag);
     });
-};
+    countTags();
+}
+
+const remove = (element, tag) => {
+    let index = tags.indexOf(tag);
+    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+    element.parentElement.remove();
+    countTags();
+}
 
 const addTag = (e) => {
-    if (e.key == 'Enter') {
+    if (e.key == "Enter") {
         let tag = e.target.value.replace(/\s+/g, ' ');
-
         if (tag.length > 1 && !tags.includes(tag)) {
-
-            tag.split(',').every(tag => {
-                tags.push(tag);
-
-                createTag();
-            });
-        };
-        e.target.value = '';
+            if (tags.length < 10) {
+                tag.split(',').forEach(tag => {
+                    tags.push(tag);
+                    createTag();
+                });
+            }
+        }
+        e.target.value = "";
     }
-};
+}
 
-input.addEventListener('keyup', addTag);
+input.addEventListener("keyup", addTag);
 
+const removeBtn = document.querySelector(".details button");
+removeBtn.addEventListener("click", () => {
+    tags.length = 0;
+    ul.querySelectorAll("li").forEach(li => li.remove());
+    countTags();
+});
+
+countTags();
+
+createTag();
