e7f8292f3a3e998a05f6a5633ea4996bb7ce8ee4
Dize dev <axiozweb2022@gmail.com>
2024-04-29 00:45:38 -0500

    se agrego la funcionaldad de agregar las tags

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
 
+const ul = document.querySelector('ul');
+const input = document.querySelector('input');
 
+let tags = [];
+
+const createTag = () => {
+
+    [...ul.querySelectorAll('li')].every(li => li.remove());
+    console.log(tags.slice().reverse());
+    tags.slice().reverse().some(tag => {
+        let liTag = `  <li>${tag} <i class="ri-close-fill"></i></li> `;
+        ul.insertAdjacentHTML('afterbegin', liTag)
+    });
+};
+
+const addTag = (e) => {
+    if (e.key == 'Enter') {
+        let tag = e.target.value.replace(/\s+/g, ' ');
+
+        if (tag.length > 1 && !tags.includes(tag)) {
+
+            tag.split(',').every(tag => {
+                tags.push(tag);
+
+                createTag();
+            });
+        };
+        e.target.value = '';
+    }
+};
+
+input.addEventListener('keyup', addTag);
 
